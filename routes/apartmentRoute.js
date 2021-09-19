e8fb8a6225e0380c78c2c1463b71f8d32f505af9
nimrodsilberfeld <nimrod513@gmail.com>
2021-09-19 09:53:29 +0300

    init

--- routes/apartmentRoute.js
+++ routes/apartmentRoute.js
@@ -6,8 +6,7 @@ const multer = require("multer")
 const multerS3 = require("multer-s3")
 const aws = require("aws-sdk")
 
-const key = "AKIAQBT7J7DV2PUSB5WQ"
-const secret = "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"
+
 
 const s3 = new aws.S3({
     credentials: {
